27cbef19235fb64f5ee87e0a64218e7416e25ba5
Hasbienk <garojr123@gmail.com>
2025-05-01 19:00:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+function toggleSection(id) {
+    const section = document.getElementById(id);
+    section.classList.toggle("hidden");
+  }
+=======
+function toggleSection(id) {
+    const section = document.getElementById(id);
+    section.classList.toggle("hidden");
+  }
+>>>>>>> c9f992b (Pindah semua file ke root)
+  
